ddf6eddcbee9f77dde66b2b6b8b484ea44a45001
TesterBoy <152272581+CryptoBuroMaster@users.noreply.github.com>
2024-10-03 08:10:56 +0530

    Update buro-setup.sh

--- buro-setup.sh
+++ buro-setup.sh
@@ -64,6 +64,41 @@ virtualenv venv
 source venv/bin/activate
 pip install vana
 
+# Create wallet
+echo "Creating wallet..."
+
+# Export coldkey private key
+print_info "Creating a new Vana wallet with coldkey..."
+vanacli wallet create --wallet.name default --wallet.coldkey default
+
+# Export hotkey private key
+print_info "Creating a new Vana wallet with hotkey..."
+vanacli wallet create --wallet.name default --wallet.hotkey default
+
+# Export private keys for coldkey
+echo "Exporting Coldkey private key..."
+if ! vanacli wallet export_private_key --wallet.name default --keytype coldkey; then
+    echo "Failed to export Coldkey private key."
+    exit 1
+fi
+
+# Export private keys for hotkey
+echo "Exporting Hotkey private key..."
+if ! vanacli wallet export_private_key --wallet.name default --keytype hotkey; then
+    echo "Failed to export Hotkey private key."
+    exit 1
+fi
+
+# Generate Encryption Keys
+echo "Generating Encryption Keys..."
+if ! ./keygen.sh; then
+    echo "Failed to generate encryption keys."
+    exit 1
+fi
+
+echo "Coldkey and Hotkey private keys exported."
+echo "Encryption keys generated: public_key.asc, public_key_base64.asc, private_key.asc, private_key_base64.asc"
+
 
 # Display output 
 echo "Process completed!"
